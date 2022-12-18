c85e3b5b07cdf63f66a371e28a63c7b1335ffc38
tkkrr <reioffitial@gmail.com>
2022-12-18 23:43:20 +0900

    Add: Sitemap.xml & postBundle Plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from 'path'
 import dns from 'dns'
 import fs from "fs"
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig, loadEnv, PluginOption } from 'vite'
 import react from '@vitejs/plugin-react'
 
 dns.setDefaultResultOrder('verbatim')
@@ -25,7 +25,7 @@ export default defineConfig(({mode}) => {
   } : {}
 
   return {
-    plugins: [react(), htmlPlugin(envs)],
+    plugins: [react(), htmlPlugin(envs), postBundledPlugin()],
     root: "src",
     envDir: envDirPath,
     build: {
@@ -47,13 +47,33 @@ export default defineConfig(({mode}) => {
 })
 
 // index.htmlに環境変数を埋込むために使用する
-function htmlPlugin(env: ReturnType<typeof loadEnv>) {
+const htmlPlugin = (env: ReturnType<typeof loadEnv>): PluginOption => {
   return {
     name: "html-transform",
     transformIndexHtml: {
-      enforce: "pre" as const,
-      transform: (html: string): string =>
+      enforce: "pre",
+      transform: html =>
         html.replace(/%(.*?)%/g, (match, p1) => env[p1] ?? match),
     },
-  };
+  }
+}
+
+// bundle処理後に処理を行うために使用する
+const postBundledPlugin = (): PluginOption => {
+  return {
+    name: 'postBundled',
+    apply: 'build',
+    closeBundle: {
+      order: 'post',
+      handler() {
+        const distSitemapPath = path.resolve(__dirname, "dist", "sitemap.xml")
+        const content = fs.readFileSync(distSitemapPath, {encoding: "utf-8"})
+        // sitemap.xmlのlastmodの値をbuild時の日付に書き換える
+        fs.writeFileSync(
+          distSitemapPath,
+          content.replace(/%INSERTED_BUILDDATE%/g, new Date().toISOString().slice(0,10))
+        )
+      }
+    }
+  }
 }
